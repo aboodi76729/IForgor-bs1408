40481f32f3a7f49d13d949616b11ed5382b57787
aboodi76729 <kingofdark327@gmail.com>
2026-05-20 09:58:19 +0300

    add include/assets.hpp

---
+++ include/assets.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include "kaleb/shared/kaleb.hpp"
+
+DECLARE_FILE(_binary_arrow_png, Assets, arrow_png);
+DECLARE_FILE(_binary_bloq_png, Assets, bloq_png);
+DECLARE_FILE(_binary_cut_arrow_png, Assets, cut_arrow_png);
+DECLARE_FILE(_binary_dot_png, Assets, dot_png);
+DECLARE_FILE(_binary_saber_bg_png, Assets, saber_bg_png);
+DECLARE_FILE(_binary_saber_fg_png, Assets, saber_fg_png);
+DECLARE_FILE(_binary_slider_png, Assets, slider_png);
+DECLARE_FILE(_binary_slider_dots_png, Assets, slider_dots_png);
