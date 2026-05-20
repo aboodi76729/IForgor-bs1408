78971d8a0e66f9b65b3862b69e568139aa383c98
aboodi76729 <kingofdark327@gmail.com>
2026-05-20 09:58:40 +0300

    add src/CustomTypes/NoteRecorder.cpp

---
+++ src/CustomTypes/NoteRecorder.cpp
@@ -0,0 +1,41 @@
+#include "CustomTypes/NoteRecorder.hpp"
+#include "UnityEngine/Resources.hpp"
+
+DEFINE_TYPE(IForgor, NoteRecorder);
+
+void IForgor::NoteRecorder::Start() 
+{
+    noteAData = nullptr;
+	noteBData = nullptr;
+}
+
+void IForgor::NoteRecorder::OnNoteWasCut(GlobalNamespace::NoteData* noteData, GlobalNamespace::NoteCutInfo noteCutInfo) 
+{
+	if (noteData == nullptr || noteData->colorType == GlobalNamespace::ColorType::None) return;
+	if (!noteCutInfo.saberTypeOK)
+	{
+		ProcessNote(noteData, std::nullopt);
+	}
+	else
+	{
+		ProcessNote(noteData, noteCutInfo);
+	}
+}
+
+void IForgor::NoteRecorder::OnNoteWasMissed(GlobalNamespace::NoteData* noteData) 
+{
+    ProcessNote(noteData, GlobalNamespace::NoteCutInfo());
+}
+void IForgor::NoteRecorder::ProcessNote(GlobalNamespace::NoteData* noteData, std::optional<GlobalNamespace::NoteCutInfo> noteCutInfo) 
+{
+    if (noteData->colorType == GlobalNamespace::ColorType::ColorA)
+	{
+		noteAData = noteData;
+		noteACutInfo = noteCutInfo;
+	}
+	else if (noteData->colorType == GlobalNamespace::ColorType::ColorB)
+	{
+		noteBData = noteData;
+		noteBCutInfo = noteCutInfo;
+	}
+}
