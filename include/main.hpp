89121f2e8cd61aa16d228a8be8379bfb9e83b7d9
aboodi76729 <kingofdark327@gmail.com>
2026-05-20 09:58:22 +0300

    add include/main.hpp

---
+++ include/main.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "CustomTypes/SaberRecorder.hpp"
+#include "CustomTypes/NoteRecorder.hpp"
+#include "CustomTypes/UIGroup.hpp"
+#include "CustomTypes/PauseUIManager.hpp"
+
+#include "scotland2/shared/modloader.h"
+
+#include "logger.hpp"
+
+#include "beatsaber-hook/shared/config/config-utils.hpp"
+#include "beatsaber-hook/shared/utils/il2cpp-functions.hpp"
+#include "beatsaber-hook/shared/utils/il2cpp-utils.hpp"
+#include "beatsaber-hook/shared/utils/hooking.hpp"
+
+//#define CHROMA
