3676fc98af0cd9cd05ca19410a466da32e46dc47
aboodi76729 <kingofdark327@gmail.com>
2026-05-20 09:58:43 +0300

    add src/CustomTypes/SaberRecorder.cpp

---
+++ src/CustomTypes/SaberRecorder.cpp
@@ -0,0 +1,40 @@
+#include "CustomTypes/SaberRecorder.hpp"
+#include "UnityEngine/Resources.hpp"
+#include "GlobalNamespace/SaberManager.hpp"
+#include "UnityEngine/Mathf.hpp"
+#include "UnityEngine/zzzz__Vector3_def.hpp"
+
+DEFINE_TYPE(IForgor, SaberRecorder);
+
+void IForgor::SaberRecorder::Init()
+{
+    if (saberRecorderInstance != nullptr) {
+        Destroy(saberRecorderInstance);
+    }
+    saberRecorderInstance = this;
+}
+
+void IForgor::SaberRecorder::Start()
+{
+    auto sabManager = UnityEngine::Resources::FindObjectsOfTypeAll<GlobalNamespace::SaberManager*>()[0];
+    _saberA = sabManager->leftSaber;
+    _saberB = sabManager->rightSaber;
+}
+
+void IForgor::SaberRecorder::RecordSaberAngles()
+{
+    RecordSaberAngle(_saberA);
+    RecordSaberAngle(_saberB);
+}
+
+void IForgor::SaberRecorder::RecordSaberAngle(GlobalNamespace::Saber* saber)
+{
+    // In BS 1.40+, saber blade position getters may be direct fields or renamed.
+    // saberBladeTopPos / saberBladeBottomPos → saberBladeTopPosition / saberBladeBottomPosition
+    auto saberVector = UnityEngine::Vector3::op_Subtraction(saber->get_saberBladeTopPos(), saber->get_saberBladeBottomPos());
+    saberVector.z = 0;
+    saberVector.Normalize();
+    float saberAngle = UnityEngine::Mathf::Atan2(saberVector.y, saberVector.x) * 57.29578f;
+    if (saber->get_saberType() == GlobalNamespace::SaberType::SaberA) saberAAngle = saberAngle;
+    if (saber->get_saberType() == GlobalNamespace::SaberType::SaberB) saberBAngle = saberAngle;
+}
